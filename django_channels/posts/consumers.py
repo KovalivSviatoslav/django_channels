bba3a921da03d54c95498863c898279eca8d4cf0
KovalivSviatoslav <kovaliv2195@gmail.com>
2021-05-30 14:17:13 +0300

    Feat: finished CommentsConsumer

--- django_channels/posts/consumers.py
+++ django_channels/posts/consumers.py
@@ -1,11 +1,17 @@
+import json
+
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.contenttypes.models import ContentType
+
+from comments.models import Comment
+from posts.models import Post
 
 
 class CommentsConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.post_id = self.scope['url_route']['kwargs']['post_id']
-        print(self.scope)
         self.post_group_name = f'post_{self.post_id}'
 
         await self.channel_layer.group_add(
@@ -13,8 +19,49 @@ class CommentsConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        await self.accept()
+
     async def disconnect(self, code):
-        await self.channel_layer.discard(
+        await self.channel_layer.group_discard(
             self.post_group_name,
-            self.channel_layer
+            self.channel_name
         )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        comment = text_data_json['text']
+
+        new_comment = await self.create_new_comment(comment)
+        data = {
+            "author": new_comment.author.username,
+            "created_at": new_comment.created_at.strftime('%Y-%m-%d %H:%m'),
+            "text": new_comment.text
+        }
+
+        await self.channel_layer.group_send(
+            self.post_group_name,
+            {
+                'type': 'new_comment',
+                'message': data
+            }
+        )
+
+    async def new_comment(self, event):
+        message = event["message"]
+
+        await self.send(
+            text_data=json.dumps({
+                "message": message
+            })
+        )
+
+    @database_sync_to_async
+    def create_new_comment(self, text):
+        ct = ContentType.objects.get_for_model(Post)
+        new_comment = Comment.objects.create(
+            author=self.scope['user'],
+            text=text,
+            content_type=ct,
+            object_id=int(self.post_id)
+        )
+        return new_comment
